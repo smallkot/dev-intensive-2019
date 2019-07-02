23d2dea9c35ee2d50f2cdaebb19351c71a68278a
Константин Позняк <smallkot@gmail.com>
2019-07-02 22:10:34 +0700

    add utils toInitials

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -0,0 +1,32 @@
+package ru.skillbranch.devintensive.extensions
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+const val SECOND = 1000L
+const val MINUTE = 60 * SECOND
+const val HOUR = 60 * MINUTE
+const val DAY = 24 * HOUR
+
+enum class TimeUnits {
+    SECOND,
+    MINUTE,
+    HOUR,
+    DAY
+}
+
+fun Date.formate(pattern: String = "HH:mm:ss dd.MM.yy"): String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
+
+fun Date.add(value: Int, timeUnits: TimeUnits): Date {
+    this.time += when(timeUnits) {
+        TimeUnits.SECOND -> value * SECOND
+        TimeUnits.MINUTE -> value * MINUTE
+        TimeUnits.HOUR -> value * HOUR
+        TimeUnits.DAY -> value * DAY
+    }
+    return this
+}
+
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -9,4 +9,18 @@ object Utils {
         if (lastName == "") lastName = null
         return Pair(firstName, lastName)
     }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        var firstNameLetter: Char? = null
+        var lastNameLetter: Char? = null
+
+        if (firstName != null && firstName.isNotEmpty()) firstNameLetter = firstName[0].toUpperCase()
+        if (lastName != null && lastName.isNotEmpty()) lastNameLetter = lastName[0].toUpperCase()
+
+        var printStr = ""
+        if (firstNameLetter != null && firstNameLetter != ' ') printStr += firstNameLetter
+        if (lastNameLetter != null && lastNameLetter != ' ') printStr += lastNameLetter
+
+        return if (printStr.isEmpty()) null else printStr
+    }
 }
