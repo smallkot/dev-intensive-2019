b23011052c492d08ee74ffc822326ea8c00f0910
Константин Позняк <smallkot@gmail.com>
2019-07-03 00:23:55 +0700

    add Паттерн Builder

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -13,11 +13,35 @@ data class User(val id : String,
                 var isOnline : Boolean = false) {
 
     companion object Factory {
-        private var indexId = 0
+        private var indexId = -1
         fun makeUser(fullName: String?): User {
             indexId++
             var (firstName, lastName) = Utils.parseFullName(fullName)
             return User("$indexId", firstName, lastName, null)
         }
+    }
+
+    data class Builder(
+        var id: String = "1",
+        var firstName : String? = null,
+        var lastName : String? = null,
+        var avatar : String? = null,
+        var rating : Int = 0,
+        var respect : Int = 0,
+        var lastVisit : Date? = null,
+        var isOnline : Boolean = false) {
+
+
+        fun id(id: String) = apply { this.id = id }
+        fun firstName(firstName: String) = apply { this.firstName = firstName}
+        fun lastName(lastName: String) = apply { this.lastName = lastName}
+        fun avatar(avatar: String) = apply { this.avatar = avatar}
+        fun rating(rating: Int) = apply { this.rating = rating}
+        fun respect(respect: Int) = apply { this.respect = respect}
+        fun lastVisit(lastVisit: Date) = apply { this.lastVisit = lastVisit}
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline}
+
+
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
     }
 }
